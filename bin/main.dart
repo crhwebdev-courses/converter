e3bdc54a396c89c0233eb465822cbf4096daf632
Carl Hain <crhain@gmail.com>
2019-06-11 17:21:16 -0500

    Add check to main.dart that exits program if user types n to first question

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
 import 'package:prompter_crh/prompter_crh.dart';
 
 void main() {
   final prompter = Prompter();
 
-  prompter.askBinary('Are you here to convet an image?');
+  final choice = prompter.askBinary('Are you here to convet an image?');
+
+  if (!choice) {
+    exit(0);
+  }
 }
