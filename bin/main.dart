d80d986328fbb408b5555117f0028793fe776030
Carl Hain <crhain@gmail.com>
2019-06-13 17:35:44 -0500

    Write and test basic buildFileOptions function in main.dart: this function gets the current working directory and prints a list of files in that directory

--- bin/main.dart
+++ bin/main.dart
@@ -11,6 +11,8 @@ void main() {
   }
 
   final format = prompter.askMultiple('Select format:', buildFormatOptions());
+  // prompter.askMultiple('Select an image to convert:', buildFileOptions())
+  buildFileOptions();
 }
 
 List<Option> buildFormatOptions() {
@@ -18,4 +20,17 @@ List<Option> buildFormatOptions() {
     Option('Convert to jpeg', 'jpeg'),
     Option('Convert to png', 'png'),
   ];
+}
+
+List<Option> buildFileOptions() {
+  // Get a reference to the current working directory
+  final currentDirectory = Directory.current;
+
+  // Find all the files and folders in this directory
+  final entities = currentDirectory.listSync();
+  print(entities);
+
+  // Look through that list and find only the *images*
+
+  // Take all the images and created an option object out of each
 }
