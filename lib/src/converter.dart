40ae3e4904ec1935304bf56bd88f88e38d5f7043
Carl Hain <crhain@gmail.com>
2019-06-17 17:23:05 -0500

    Impliment convertImage function

--- lib/src/converter.dart
+++ lib/src/converter.dart
@@ -1,4 +1,22 @@
 import 'dart:io';
 import 'package:image/image.dart';
 
-convertImage(FileSystemEntity selectedFile, String format) {}
+convertImage(FileSystemEntity selectedFile, String format) {
+  //read type cast file as bytes
+  final rawImage = (selectedFile as File).readAsBytesSync();
+
+  //decode the image
+  final image = decodeImage(rawImage);
+
+  var newImage;
+
+  if (format == 'jpg') {
+    newImage = encodeJpg(image);
+  } else if (format == 'png') {
+    newImage = encodePng(image);
+  } else {
+    print('Unsupported file type');
+  }
+
+  File('lkalksdjfj').writeAsBytesSync(newImage);
+}
